bc881fc5e9d14f7eee79a86ad52dbce35fee35c9
Sigurd Kjelsbøl Huse <sigurdkh1234@gmail.com>
2023-09-06 14:13:04 +0200

    Made regression methods

--- Project 1/regression.py
+++ Project 1/regression.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 
 def FrankeFunction(x, y):
@@ -14,12 +17,16 @@ class GeneralRegression:
     """Master class for all regression problems"""
 
     def __init__(
-        self, x: np.array, y: np.array, f: np.array, degree: int, treshold: float
+        self, x: np.array, y: np.array, z: np.array, degree: int, treshold: float
     ) -> None:
         self.x = x
         self.y = y
-        self.f = f
+        self.z = z
+        # self.n = y.size
         self.treshold = treshold
+
+        self.predicted_train = None
+        self.predicted_test = None
 
         self.degree = degree
 
@@ -27,30 +34,87 @@ class GeneralRegression:
         self.split_training_and_test(treshold)
 
     def make_design_matrix(self, x: np.array, y: np.array, degree: int) -> None:
-        self.nr_of_params = (degree + 1) * (degree + 1)
+        self.nr_of_params = (degree + 1) * (degree + 1) - 1
         self.X = np.zeros((x.size, (degree + 1) * (degree + 1)))
         idx = 0
 
-        for i in range(degree + 1):
-            for j in range(degree + 1):
-                self.X[:, idx] = x ** (i) * y ** (j)
+        # If degree = 1, we want [x, y]
+        # If degree = 2, we want [x,y, x^2, y^2, xy]
+
+        for j in range(0, degree + 1):
+            for i in range(0, degree + 1):
+                self.X[:, idx] = (x ** (i)) * (y ** (j))
                 idx += 1
+
+        self.X = self.X[:, 1:]
+        # self.X = preprocessing.MinMaxScaler().fit_transform(self.X)
 
     def split_training_and_test(self, treshold: float):
         from sklearn.model_selection import train_test_split
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            self.X, self.y, test_size=treshold
+        self.X_train, self.X_test, self.z_train, self.z_test = train_test_split(
+            self.X, self.z, test_size=treshold
         )
 
-    def MSE(self):
-        pass
+    def MSE(self, on_training: bool) -> float:
+        if on_training:
+            return (
+                np.sum((self.z_train - self.predicted_train) ** 2) / self.z_train.size
+            )
 
     def R2(self):
         pass
 
 
+class OSLpredictor(GeneralRegression):
+    def __init__(
+        self, x: np.array, y: np.array, f: np.array, degree: int, treshold: float
+    ) -> None:
+        super().__init__(x, y, f, degree, treshold)
+
+    def compute_parameters(self):
+        self.params = (
+            np.linalg.inv(self.X_train.T @ self.X_train) @ self.X_train.T
+        ) @ self.z_train
+
+
+class Ridgepredictor(GeneralRegression):
+    def __init__(
+        self, x: np.array, y: np.array, f: np.array, degree: int, treshold: float
+    ) -> None:
+        super().__init__(x, y, f, degree, treshold)
+
+    def compute_parameters(self, lam: float):
+        n = self.X_train.shape[1]
+        # Formula from lecture
+        self.params = (
+            np.linalg.inv(self.X_train.T @ self.X_train + lam * np.eye(n))
+            @ self.X_train.T
+        ) @ self.z_train
+
+
 if __name__ == "__main__":
     n = 100
-    test = GeneralRegression(np.zeros(n), np.zeros(n), np.zeros(n), 1, 0.2)
-    print(test.X_test.shape)
+
+    x = np.arange(0, 1, 0.05)
+    y = np.arange(0, 1, 0.05)
+    x, y = np.meshgrid(x, y)
+    z = FrankeFunction(x, y)
+
+    # print((x * y**0).flatten())
+    test = OSLpredictor(x.flatten(), y.flatten(), z.flatten(), 6, 0.2)
+    test.compute_parameters()
+    fig = plt.figure()
+    ax = fig.gca(projection="3d")
+    surf = ax.plot_surface(
+        x,
+        y,
+        (test.X @ test.params).reshape((20, 20)),
+        cmap=cm.coolwarm,
+        linewidth=0,
+        antialiased=False,
+    )
+    plt.show()
+    # print(
+    #    np.sum(((test.X_train @ test.params) - test.z_train) ** 2) / test.z_train.size
+    # )
