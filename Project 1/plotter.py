0e0b84073c0565315a8364b36a41404d35011521
Sigurd Kjelsbøl Huse <sigurdkh1234@gmail.com>
2023-09-13 11:50:16 +0200

    Added docs to plotter.py

--- Project 1/plotter.py
+++ Project 1/plotter.py
@@ -28,6 +28,25 @@ def plot_MSE_or_R2_as_func_of_degree(
     plot_MSE: bool,
     plot_with_lam: bool,
 ) -> None:
+    """Plots the MSE or R2 as a function of maximum degree of polynomials in design matrix.
+
+    Args:
+        x (np.array): x-coordinates.
+        y (np.array): y-coordinates.
+        z (np.array): Values to predict, also z-coordinates.
+        nr_of_degs (int): Plots the degrees up to this degree.
+        threshold (float): Treshold used to split data.
+        scale (bool): Determines if data is scaled.
+        lam (list): List of lambda values to compute MSE and R2 for.
+        filename (str): Filename to save plot as.
+        regression_type (str): Which type of regression to be used.
+                               It is only possible to use OLS, Ridge and Lasso.
+        plot_MSE (bool): True if MSE is to be plotted, False if R2 should be plotted.
+        plot_with_lam (bool): True if lambda values are to be part of label.
+
+    Raises:
+        NotImplementedError: Raised if a regression type which is not implmented is passed as regression_type.
+    """
     nr_of_degs = int(nr_of_degs)
     degrees = list(range(1, nr_of_degs + 1))
     results_test = np.zeros((len(lam), nr_of_degs))
@@ -79,6 +98,7 @@ def plot_MSE_or_R2_as_func_of_degree(
         ax1.set(ylabel="R2-score", xlabel="Degree")
         ax2.set(ylabel="R2-score", xlabel="Degree")
 
+    fig.suptitle(f"Regression using {regression_type}")
     ax1.set(title="Training data")
     ax2.set(title="Test data")
 
@@ -89,6 +109,10 @@ def plot_MSE_or_R2_as_func_of_degree(
     ax1.legend()
     ax2.legend()
     plt.savefig(dir_name + "/" + filename)
+
+
+def plot_MSE_using_bootstrap_and_OSL(nr_of_its, nr_of_degs) -> None:
+    pass
 
 
 if __name__ == "__main__":
@@ -103,16 +127,52 @@ if __name__ == "__main__":
     x, y = np.meshgrid(x, y)
     z = FrankeFunction(x, y)
 
-    plot_MSE_or_R2_as_func_of_degree(
-        x=x.flatten(),
-        y=y.flatten(),
-        z=z.flatten(),
-        nr_of_degs=12,
-        threshold=0.2,
-        scale=True,
-        lam=[0],
-        filename="MSE_OSL.png",
-        regression_type="OLS",
-        plot_MSE=True,
-        plot_with_lam=True,
-    )
+    osl_plots = [(True, "MSE_OSL.png"), (False, "R2_OSL.png")]
+
+    for i in osl_plots:
+        plot_MSE_or_R2_as_func_of_degree(
+            x=x.flatten(),
+            y=y.flatten(),
+            z=z.flatten(),
+            nr_of_degs=10,
+            threshold=0.2,
+            scale=True,
+            lam=[0],
+            filename=i[1],
+            regression_type="OLS",
+            plot_MSE=i[0],
+            plot_with_lam=False,
+        )
+    lambdas = [1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
+    ridge_plots = [(True, "MSE_Ridge.png"), (False, "R2_Ridge.png")]
+    for i in ridge_plots:
+        plot_MSE_or_R2_as_func_of_degree(
+            x=x.flatten(),
+            y=y.flatten(),
+            z=z.flatten(),
+            nr_of_degs=10,
+            threshold=0.2,
+            scale=True,
+            lam=lambdas,
+            filename=i[1],
+            regression_type="Ridge",
+            plot_MSE=i[0],
+            plot_with_lam=True,
+        )
+
+    alphas = [0.1, 0.01, 0.001, 0.0001]
+    lasso_plots = [(True, "MSE_Lasso.png"), (False, "R2_Lasso.png")]
+    for i in lasso_plots:
+        plot_MSE_or_R2_as_func_of_degree(
+            x=x.flatten(),
+            y=y.flatten(),
+            z=z.flatten(),
+            nr_of_degs=10,
+            threshold=0.2,
+            scale=True,
+            lam=alphas,
+            filename=i[1],
+            regression_type="Lasso",
+            plot_MSE=i[0],
+            plot_with_lam=True,
+        )
